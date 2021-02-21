8d6a32af2630e1d5d7e5c560b5315b7fd91901bd
dannybd <dannybd@mit.edu>
2021-02-21 08:17:02 +0000

    garlic command

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python3
 
+import asyncio
 import datetime
 import discord
 import json
@@ -41,7 +42,7 @@ default_help = commands.DefaultHelpCommand(
 
 
 bot = commands.Bot(
-    command_prefix="carlson ",
+    command_prefix=["carlson ", "Carlson "],
     description=(
         "Carl Carlson the Second, Son of Carl: "
         + "a Discord bot for the Hawai'i Fridays"
@@ -54,6 +55,30 @@ bot = commands.Bot(
 @bot.event
 async def on_ready():
     logging.info("Connected as {0.user} and ready!".format(bot))
+
+
+@bot.command(name="punish", hidden=True)
+async def punish(ctx, *, member: typing.Optional[discord.Member]):
+    if ctx.author.id not in [276520565458862081, 448896411036024832]:
+        return
+    if not member:
+        await ctx.send("Couldn't find a user with that name")
+        return
+    await ctx.send("{}, you have crossed garlic for the last time".format(member.mention))
+    await asyncio.sleep(1)
+    await ctx.send("now")
+    await asyncio.sleep(1)
+    await ctx.send("_pewish_")
+    nick, *rest = member.display_name.split("(")
+    nick = re.sub(r'[lr]', 'w', nick)
+    nick = re.sub(r'[LR]', 'W', nick)
+    nick = re.sub(r'n([aeiou])', r'ny\1', nick)
+    nick = re.sub(r'N([aeiou])', r'Ny\1', nick)
+    nick = re.sub(r'N([AEIOU])', r'NY\1', nick)
+    nick = re.sub(r'ove', 'uv', nick)
+    nick = re.sub(r'OVE', 'UV', nick)
+    nick = nick + "(" + " ".join(rest)
+    await member.edit(nick=nick)
 
 
 logging.info("Loading extensions...")
